e2eeb9ae41c984f06242d2c083c1558d83ff7a62
IRONKAGE <IRONKAGE@gmail.com>
2020-03-23 23:11:32 +0200

    add cod and add comments in Selection Sort

--- Python/Selection_Sort_Function.py
+++ Python/Selection_Sort_Function.py
@@ -1,20 +1,22 @@
 from random import randint
 
+# logic of Algorithm [логіка алгоритму]
 def selection_sort(array):
-    for index in range(len(array) - 1):
-        minimum = index
-        newIndex = index + 1
-        while newIndex < len(array):
-            if array[newIndex] < array[minimum]:
-                minimum = newIndex
-            newIndex = newIndex + 1
-        array[index], array[minimum] = array[minimum], array[index]
+    for horizontal_index in range(len(array) - 1):
+        minimum = horizontal_index
+        vertical_index = horizontal_index + 1
+        while vertical_index < len(array):
+            if array[vertical_index] < array[minimum]:
+                minimum = vertical_index
+            vertical_index = vertical_index + 1
+        array[horizontal_index], array[minimum] = array[minimum], array[horizontal_index]
 
+# test case [Тестовий випадок]
 N = 10
 a = []
 for i in range(N):
     a.append(randint(0, 99))
 
-print(a)
+print(a) # Random array [Випадковий масив]
 selection_sort(a)
-print(a)
+print(a) # Sorted array [Відсортований масив]
