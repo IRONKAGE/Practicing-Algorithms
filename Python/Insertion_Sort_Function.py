df3cdd8f66c8e6497c1b94f4bd340aabaadb5369
IRONKAGE <IRONKAGE@gmail.com>
2020-03-23 23:10:57 +0200

    fix cod and add comments in Insertion Sort

--- Python/Insertion_Sort_Function.py
+++ Python/Insertion_Sort_Function.py
@@ -1,18 +1,21 @@
 from random import randint
 
+# logic of Algorithm [логіка алгоритму]
 def insertionSort(array):
-    for index in range(N):
-        currentvalue = array[index]
-        while index > 0 and array[index - 1] > currentvalue:
-            array[index] = array[index - 1]
-            index = index - 1
-        array[index] = currentvalue
+    for horizontal_index in range(len(array)):
+        current_value = array[horizontal_index]
+        vertical_index = horizontal_index
+        while vertical_index > 0 and array[vertical_index - 1] > current_value:
+            array[vertical_index] = array[vertical_index - 1]
+            vertical_index = vertical_index - 1
+        array[vertical_index] = current_value
 
+# test case [Тестовий випадок]
 N = 10
 a = []
 for index in range(N):
     a.append(randint(0, 99))
 
-print(a)
+print(a) # Random array [Випадковий масив]
 insertionSort(a)
-print(a)
+print(a) # Sorted array [Відсортований масив]
